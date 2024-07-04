ef5a7ecdfa1b3904f84aef0fc146003d615b55bb
Souvik Das <souvikdascoder@gmail.com>
2024-07-04 23:45:33 +0530

    add video backgroun with autoplay and compelte the Carousel.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,18 +4,20 @@ import Navbar from '../../components/Navbar/Navbar'
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { FaPlay, FaInfo} from "react-icons/fa"
+import { FaPlay, FaInfo } from "react-icons/fa"
 import Data from '../../data.json'
 
 function Home() {
   var settings = {
     dots: true,
     infinite: true,
-    speed: 500,
+    speed: 300,
     slidesToShow: 1,
     slidesToScroll: 1,
     initialSlide: 0,
     arrows: false,
+    autoplay: true,
+    autoplaySpeed: 30000,
     responsive: [
       {
         breakpoint: 1024,
@@ -43,28 +45,36 @@ function Home() {
       }
     ]
   };
+
   return (
     <div className='home'>
-      <Navbar/>
+      <Navbar />
       <Slider {...settings}>
-        {Data.map(index =>(
-        <div className="hero">
-          <img src={index.background} alt="" className='banner-img'/>
-          <div className="hero-caption">
-            <img src={index.bsp} alt="bingespecial" className='bsp'/>
-            <img src={index.title} alt="title" className='caption-img' />
-            <p className='details'>{index.details}</p>
-            <p>{index.description}</p>
-            <p className='details'>{index.mcategory}</p>
-            <div className="hero-btns">
-              <button className='btn'><FaPlay className='btn-icon'/>Play</button>
-              <button className='btn2'><FaInfo className='btn-icon'/>More info</button>
+        {Data.map((item, index) => (
+          <div className="hero" key={index}>
+            <video
+              src={item.video}
+              className='banner-video'
+              autoPlay
+              muted
+              playsInline
+              loop
+            />
+            <div className="hero-caption">
+              <img src={item.bsp} alt="bingespecial" className='bsp' />
+              <img src={item.title} alt="title" className='caption-img' />
+              <p className='details'>{item.details}</p>
+              <p>{item.description}</p>
+              <p className='details'>{item.mcategory}</p>
+              <div className="hero-btns">
+                <button className='btn'><FaPlay className='btn-icon' />Play</button>
+                <button className='btn2'><FaInfo className='btn-icon' />More info</button>
+              </div>
             </div>
           </div>
-        </div>
         ))}
       </Slider>
-  </div>
+    </div>
   )
 }
 
